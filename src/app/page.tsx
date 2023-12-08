cda91b01ca558bf3a0f04b7782157d2d361bab46
siljapetasch <siljatpetasch@gmail.com>
2023-12-08 15:08:46 +0100

    complete fiat on ramp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
-import Image from 'next/image'
+"use client";
+
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { Appearance, StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
+import {ConnectWallet, embeddedWallet, MetaMaskWallet, ThirdwebProvider, useAddress, useBalance, useBalanceForAddress, useContract, useTokenSupply} from "@thirdweb-dev/react";
+import { useState } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <ThirdwebProvider
+      activeChain="avalanche-fuji"
+      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      supportedWallets={[embeddedWallet()]}
+    >
+      <ClaimPage />
+    </ThirdwebProvider>
+  );
+}
+
+function ClaimPage() {
+  const address = useAddress();
+  const [clientSecret, setClientSecret] = useState<string>("")
+
+  const {contract} = useContract(process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS, "token");
+  const { data: tokenSupplyData} = useTokenSupply(contract);
+  const { data: balanceOfData } = useBalance(contract?.getAddress());
+
+  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+    throw 'Did you forget to add a ".env.local" file?';
+  }
+
+  const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+  const appearance: Appearance = {
+    theme: "flat"
+  }
+  const options: StripeElementsOptions = {
+    clientSecret,
+    appearance
+  }
+
+  const onClick = async () => {
+    const resp = await fetch("/api/stripe-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        buyerWalletAddress: address,
+      }),
+    });
+    if (resp.ok) {
+      const json = await resp.json();
+      setClientSecret(json.clientSecret);
+    }
+  };
+
+  return (
+    <main className="flex flex-col gap-y-8 items-center p-12 h-screen">
+      <h1 className="text-6xl font-bold">Fiat Onramp</h1>
+      <h1 className="text-xl">Buy FAN with Credit Card</h1>
+      <ConnectWallet theme={"light"} />
+      <div>
+      <p>Your Balance: {balanceOfData?.displayValue.toString()} {balanceOfData?.symbol}</p>
+      <p>Total Supply: {tokenSupplyData?.displayValue.toString()} {tokenSupplyData?.symbol}</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {!clientSecret ? (
+            <button
+              className="bg-[#112D4E] hover:bg-[#3F72AF] text-white font-bold py-2 px-4 rounded-lg disabled:bg-gray-400 disabled:opacity-50"
+              onClick={onClick}
+              disabled={!address}
+            >
+              Buy Tokens
+            </button>
+          ) : (
+            <Elements
+              options={{
+                clientSecret,
+                appearance: { theme: "night" },
+              }}
+              stripe={stripe}
+            >
+              <CreditCardForm />
+            </Elements>
+          )}
     </main>
   )
 }
+
+const CreditCardForm = () => {
+  const elements = useElements();
+  const stripe = useStripe();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const onClick = async () => {
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const { paymentIntent, error } = await stripe.confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: "http://localhost:3000/",
+        },
+        redirect: "if_required",
+      });
+      if (error) {
+        throw error.message;
+      }
+      if (paymentIntent.status === "succeeded") {
+        setIsCompleted(true);
+      } else {
+        alert("Payment failed. Please try again.");
+      }
+    } catch (e) {
+      alert(`There was an error with the payment. ${e}`);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+    {!isCompleted ?
+    <>
+      <PaymentElement />
+
+      <button
+        className= "bg-[#112D4E] hover:bg-[#3F72AF] text-white font-bold py-2 px-4 rounded-lg"
+        onClick={onClick}
+        disabled={isLoading || isCompleted || !stripe || !elements}
+      >
+        {isLoading
+          ? "Please wait..."
+          : "Pay now"}
+      </button>
+      </>
+    : <p className="font-semibold text-lg">Thank you for your payment. You will receive your Tokens shortly.</p>}
+    </>
+  );
+};
